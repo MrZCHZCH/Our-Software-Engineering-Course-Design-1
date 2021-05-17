7924256e61478b9ec741a0d679c7e2f7e8d4e882
zch <462663932@qq.com>
2021-05-17 16:21:37 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8088'
+axios.defaults.timeout = 1000
 
 const app = createApp(App)
 app.use(ElementPlus)
