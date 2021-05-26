abfdadd7c27e44e5a7c58c66b1152ba1df53cefb
zch <462663932@qq.com>
2021-05-26 20:40:10 +0800

    App.vue展示目前已完成组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <log-in-page/>
+  <register-page></register-page>
   <EListPage :user-id="1" :user-type="1"/>
   <Editor :user-id="1" :user-type="1"/>
 </template>
@@ -9,9 +10,11 @@
 import Editor from "@/components/Editor";
 import EListPage from "@/components/EListPage";
 import LogInPage from "@/components/LogInPage";
+import RegisterPage from "@/components/RegisterPage";
 export default {
   name: 'App',
   components: {
+    RegisterPage,
     LogInPage,
     EListPage,
     Editor
@@ -25,7 +28,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
 }
